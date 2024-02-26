bc7697368cccf7efb31cfb6085953ca9298af52d
cd <lesleyrotonto@gmail.com>
2024-02-25 20:40:50 -0500

    add contact email form and about me page

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
+
+const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_cp85x5q', 'template_hn8bh7v', form.current, {
+        publicKey: 'PdLJL5BOtnZbfX1cz',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+    e.target.reset()
+  };
+
+  return (
+    <div className='border-2 rounded-lg border-dotted border-[#4A4453] p-4 text-[#4A4453]'>
+      <form ref={form} onSubmit={sendEmail} className="bg-transparent col-span-4">
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+          <div>
+            <label>Name</label>
+            <input type="text" name="name" className="bg-transparent border-b border-[#4A4453] w-full" />
+          </div>
+          <div>
+            <label>Email</label>
+            <input type="email" name="email" className="bg-transparent border-b border-[#4A4453] w-full" />
+          </div>
+          <div className='md:col-span-2'>
+            <label>Message</label>
+            <textarea name="message" className="bg-transparent border-b border-[#4A4453] w-full h-32" />
+          </div>
+          <div className='md:col-span-2'>
+            <button className='hover:underline w-full' type="submit">Send</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+export default Contact
