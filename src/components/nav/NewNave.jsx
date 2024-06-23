c726b18692461ad74a4f201bfe7325e997053cd9
cd <lesleyrotonto@gmail.com>
2024-06-22 22:19:38 -0400

    menu button adjusted

--- src/components/nav/NewNave.jsx
+++ src/components/nav/NewNave.jsx
@@ -40,15 +40,15 @@ const NewNav = () => {
                         <img className='h-8' src={github} alt='github' />
                     </a>
                 </div>
-                <button onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <button onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-black dark:text-black dark:focus:ring-black" aria-controls="navbar-default" aria-expanded="false">
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                     </svg>
                 </button>
                 <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-black dark:bg-opacity-75 md:dark:bg-transparent dark:border-gray-700">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-black rounded-lg bg-customBrown-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-customBrown dark:bg-opacity-75 md:dark:bg-transparent dark:border-black">
                         {navLinks.map((link, linkIndx) => (
-                            <li key={linkIndx} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">
+                            <li key={linkIndx} className="block py-2 px-3 text-black-900 rounded hover:bg-customBrown-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:underline dark:hover:text-black md:dark:hover:bg-transparent">
                                 <a href={`#${link.toLowerCase()}`} onClick={(event) => handleSmoothScroll(event, link)}>
                                     {link}</a>
                             </li>
