46fd9d8802329af4e6e1c06a8cb5bebd37c7dae1
cd <lesleyrotonto@gmail.com>
2024-07-05 21:40:29 -0400

    nav adjuested

--- src/components/nav/NewNave.jsx
+++ src/components/nav/NewNave.jsx
@@ -24,21 +24,21 @@ const NewNav = () => {
 
     return (
         <nav className="bg-black bg-opacity-0 fixed w-full z-20 top-0 start-0 ">
-            <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <div className="flex items-center space-x-3 rtl:space-x-reverse">
                     <Link className="flex items-center space-x-3 rtl:space-x-reverse" to='/' onClick={() => {
                         window.scrollTo(0, 0)
                     }}>
-                        <img className="h-10 w-10 md:h-20 w-20" src={logo} alt="logo" />
-                        <p className="hidden md:block">Lesley Rotonto</p>
+                        <img className="h-10 w-10 md:h-40 w-40" src={logo} alt="logo" />
+                        <p className="hidden md:block md:text-xl">Lesley Rotonto</p>
                     </Link>
                 </div>
                 <div className="flex items-center space-x-5 items-center">
                     <a className="flex items-center hover:animate-lift-up" href="https://www.linkedin.com/in/lesley-rotonto/">
-                        <img className='h-5 md:h-8' src={linkedin} alt='linkedin' />
+                        <img className='h-5 md:h-16' src={linkedin} alt='linkedin' />
                     </a>
                     <a className="flex items-center hover:animate-lift-up " href="https://github.com/GlitterAngle">
-                        <img className='h-5 md:h-8' src={github} alt='github' />
+                        <img className='h-5 md:h-16' src={github} alt='github' />
                     </a>
                 </div>
                 <button onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-black dark:text-black dark:focus:ring-black" aria-controls="navbar-default" aria-expanded="false">
@@ -49,7 +49,7 @@ const NewNav = () => {
                 <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-black rounded-lg bg-customBrown-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-customBrown dark:bg-opacity-75 md:dark:bg-transparent dark:border-black">
                         {navLinks.map((link, linkIndx) => (
-                            <li key={linkIndx} className="block py-2 px-3 text-black-900 rounded hover:bg-customBrown-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-black dark:hover:underline dark:hover:text-black md:dark:hover:bg-transparent">
+                            <li key={linkIndx} className="block py-2 px-3 text-black-900 rounded hover:bg-customBrown-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-black dark:hover:underline dark:hover:text-black md:dark:hover:bg-transparent md:text-xl">
                                 <a href={`#${link.toLowerCase()}`} onClick={(event) => handleSmoothScroll(event, link)}>
                                     {link}</a>
                             </li>
